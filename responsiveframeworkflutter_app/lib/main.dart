cb477d2ae8f5f14182752501868501b05ec3f230
dandy <danielsbarbosa27@gmail.com>
2021-03-12 16:47:45 -0300

    Criando o projeto

---
+++ responsiveframeworkflutter_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:responsiveframeworkflutter_app/pages/home/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          ClampingScrollWrapper.builder(context, widget),
+          defaultScale: true,
+          minWidth: 450,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.resize(700, name: TABLET),
+            ResponsiveBreakpoint.resize(700, name: DESKTOP),
+          ]),
+      home: HomePage(),
+    );
+  }
+}
